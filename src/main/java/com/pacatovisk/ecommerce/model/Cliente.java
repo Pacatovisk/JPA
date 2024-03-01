3ad847420bbc8b5821f4cfc2b739ead89cf21543
Pacatovisck <marcelovianadiaspacato@gmail.com.br>
2024-02-29 22:43:55 -0300

    feat: lombok

--- src/main/java/com/pacatovisk/ecommerce/model/Cliente.java
+++ src/main/java/com/pacatovisk/ecommerce/model/Cliente.java
@@ -3,41 +3,23 @@ package com.pacatovisk.ecommerce.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
 
 import java.util.Objects;
 
+@Getter
+@Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "cliente")
 public class Cliente {
 
     @Id
+    @EqualsAndHashCode.Include
     private Integer id;
 
     private String nome;
-
-    public Cliente(Integer id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
-
-    public Cliente() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/com/pacatovisk/ecommerce/model/Produto.java
+++ src/main/java/com/pacatovisk/ecommerce/model/Produto.java
@@ -3,11 +3,19 @@ package com.pacatovisk.ecommerce.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.Objects;
 
+@Getter
+@Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "produto")
 public class Produto {
 
@@ -19,38 +27,6 @@ public class Produto {
     private String descricao;
 
     private BigDecimal preco;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
-    }
 
     @Override
     public boolean equals(Object o) {
