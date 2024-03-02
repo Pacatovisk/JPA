cd48c4ae11ffdd5a8e056760f042a3f4f6c4df54
Pacatovisck <marcelovianadiaspacato@gmail.com.br>
2024-03-02 20:02:36 -0300

    test: estratégias para chave primaria com generatedvalue

---
+++ src/test/java/com/pacatovisk/ecommerce/mapeamentobasico/EstrategiaChavePrimariaTest.java
@@ -0,0 +1,23 @@
+package com.pacatovisk.ecommerce.mapeamentobasico;
+
+import com.pacatovisk.ecommerce.iniciandocomjpa.EntityManagerTest;
+import com.pacatovisk.ecommerce.model.Categoria;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EstrategiaChavePrimariaTest extends EntityManagerTest {
+
+    @Test
+    public void testarEstrategiaAuto() {
+        Categoria categoria = new Categoria();
+        categoria.setNome("Eletrônicos");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(categoria);
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+        Assert.assertNotNull(categoriaVerificacao);
+    }
+}
