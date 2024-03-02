a26ac515573802c898ad0b00ec3f967599c694fa
Pacatovisck <marcelovianadiaspacato@gmail.com.br>
2024-03-02 20:32:25 -0300

    test: estratégias para chave primaria com generatedvalue - TABLE

--- src/main/java/com/pacatovisk/ecommerce/model/Categoria.java
+++ src/main/java/com/pacatovisk/ecommerce/model/Categoria.java
@@ -17,8 +17,13 @@ import java.io.Serializable;
 public class Categoria implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
-    @SequenceGenerator(name  = "seq", sequenceName = "sequencias_chave_primaria")
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "tabela")
+    @TableGenerator(name  = "tabela", table = "hibernate_sequences",
+                    pkColumnName = "sequence_name",
+                    pkColumnValue = "categoria",
+                    valueColumnName = "next_val",
+                    initialValue = 0,
+                    allocationSize = 50)
     private Integer id;
 
     @Column(name = "nome")
